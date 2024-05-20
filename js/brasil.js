e8c1e99d61586fb9a54dbd742b55573554d5ffaf
ArielAricoma <ariel16ary@gmail.com>
2024-05-20 18:41:16 -0300

    cambio

---
+++ js/brasil.js
@@ -0,0 +1,31 @@
+var imagenes = ['/imagenes/comidas Brasileras/quindim.jpg','/imagenes/comidas Brasileras/quindim1.png','/imagenes/comidas Brasileras/quindim2.png',
+'/imagenes/comidas Brasileras/quindim3.png','/imagenes/comidas Brasileras/quindim4.png'
+    ];
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    let cont = 0;
+    let contenedor = document.querySelector('.contenedor')
+    contenedor.addEventListener('click', e => {
+        let atras = contenedor.querySelector('.atras'),
+            siguiente = contenedor.querySelector('.siguiente'),
+            img = contenedor.querySelector('img'),
+            tgt = e.target;
+        if (tgt === atras) {
+            if (cont > 0) {
+                img.src = imagenes[cont - 1];
+                cont--;
+            } else {
+                img.src = imagenes[imagenes.length - 1];
+                cont = imagenes.length - 1;
+            }
+        } else if (tgt === siguiente) { 
+            if (cont + 1 < imagenes.length) {
+                img.src = imagenes[cont + 1];
+                cont++;
+            } else {
+                img.src = imagenes[0];
+                cont = 0;
+            }
+        }
+    });
+});
